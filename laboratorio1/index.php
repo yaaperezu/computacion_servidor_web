5644b2d47a987b632b3600e95f9f5d93aafab1e4
Edwin <edwin_gb92@hotmail.com>
2018-05-17 21:11:22 -0500

    Actualización Index

--- laboratorio1/index.php
+++ laboratorio1/index.php
@@ -5,53 +5,83 @@
         
         <meta charset="utf-8" />
         
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
-        <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
-        <link rel="stylesheet" href="assets/css/style.css" />
-        
-        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+        <link rel="stylesheet" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
 	</head>
     <body>
-        
-    <div class="container">
-    
-    <?php
-    $controller = 'empleado';
 
-    // Todo esta lógica hara el papel de un FrontController
-    if( !isset($_REQUEST['c']) ) {
-        require_once "controllers/$controller.controller.php";
-        $controller = ucwords($controller) . 'Controller';
-        $controller = new $controller;
-        $controller->Index();    
-    } else {
-        // Obtenemos el controlador que queremos cargar
-        $controller = strtolower($_REQUEST['c']);
-        $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
-        
-        // Instanciamos el controlador
-        require_once "controllers/$controller.controller.php";
-        $controller = ucwords($controller) . 'Controller';
-        $controller = new $controller;
-        
-        // Llama la accion
-        call_user_func( array( $controller, $accion ) );
-    }
+
+    <div class="container">  
+  
+    <?php
+require_once 'modelo/basedatos.php';
+
+$controller = 'empleado';
+
+// Todo esta lógica hara el papel de un FrontController
+if(!isset($_REQUEST['c']))
+{
+    require_once "controller/$controller.controller.php";
+    $controller = ucwords($controller) . 'Controller';
+    $controller = new $controller;
+    $controller->Index();    
+}
+else
+{
+    // Obtenemos el controlador que queremos cargar
+    $controller = strtolower($_REQUEST['c']);
+    $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'Index';
+    
+    // Instanciamos el controlador
+    require_once "controller/$controller.controller.php";
+    $controller = ucwords($controller) . 'Controller';
+    $controller = new $controller;
+    
+    // Llama la accion
+    call_user_func( array( $controller, $accion ) );
+}
 ?>
-            <div class="row">
-                <div class="col-xs-12">
+            <div class="col-md-12">
+                
                     <hr />
-                    <footer class="text-center well">
-                        <p>Laboratorio1 Computación en el Servidor Web <br/>
-                        Estudiantes UNIR</a></p>
+                    <footer class="text-center">
+                        <p>Estudiantes UNIR</a></p>
                     </footer>                
-                </div>    
+             
             </div>
         </div>
+       
+        <script src="bower_components\jquery-dist\jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+        <script src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+       
 
-        <script src="assets/js/bootstrap.min.js"></script>
-        <script src="assets/js/jquery-ui/jquery-ui.min.js"></script>
+<script>$(document).ready( function () {
+    $('#tablaempleado').DataTable({
+        "lengthMenu": [[5, 10, 25, -1], [5, 10, 25, "Todo"]],
+        "language": {
+            "lengthMenu": "Muestra _MENU_ registros por página",
+            "zeroRecords": "No se encontró nada, lo siento",
+            "info": "Mostrando la página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(filtrado de _MAX_ registros totales)",
+            "search": "Buscar:",
+            "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"}
+        }
+    
+    });
+    
+} );</script>
+        
+    </body>
+</html>s/js/jquery-ui/jquery-ui.min.js"></script>
         <script src="assets/js/ini.js"></script>
         <script src="assets/js/jquery.anexsoft-validator.js"></script>
     </body>
